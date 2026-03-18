e9cb0843eb0dd985dfffd0158a457e5803d61927
achmadanval <anvalalan@gmail.com>
2026-03-18 10:27:01 +0700

    Jobsheet 5

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('email')
+                    ->email()
+                    ->unique(ignoreRecord:true)
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('password')
+                    ->password()
+                    ->minLength(6)
+                    ->required(),
+                    
+            ]);
+    }
+}
